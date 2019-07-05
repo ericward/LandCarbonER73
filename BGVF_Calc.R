86befd73b9d022312c49ebd745f29827fcf7703a
Ward <eward@usgs.gov>
2019-07-05 15:09:53 -0500

    Updating NPP calcs, added notes about Smalley Method

--- BGVF_Calc.R
+++ BGVF_Calc.R
@@ -1,19 +1,58 @@
 PrevCal<-read.csv('LUCAS_Stocks_ER73_Stagg_201906.csv')
 SoilCores<-read.csv('LUCAS_Soil_Cores_ER73_Baustian_201906.csv')
 
+#Convert depth range to minimum maximum depth
+max.depth.cm<-numeric()
+min.depth.cm<-numeric()
+for(i in 1:dim(SoilCores)[1]){
+  xx<-strsplit(as.character(SoilCores[i,4]),'-')
+  min.depth.cm<-c(min.depth.cm,unlist(xx)[1])
+  max.depth.cm<-c(max.depth.cm,unlist(xx)[2])
+}
+
+
+#check intervals are always 2 cm
+
+
 #calculate TC 0-30 cm g m-2
-TC.g.m.2<-SoilCores[,9]*2*10^4 #convert g cm-3 to g m-2 ground area for 2 cm depth intervals
-SoilCores<-cbind(SoilCores,TC.g.m.2)
+TC.g.m.2.per.cm<-SoilCores[,9]*10^4 #convert g cm-3 to g m-2 ground area per 1 cm depth interval
+SoilCores<-cbind(SoilCores,min.depth.cm,max.depth.cm,TC.g.m.2.per.cm)
 uID<-unique(SoilCores$Site)
 
-BGVF<-matrix(NA,2,1)
-BGVF[1,1]<-uID[1]
-xx<-which(SoilCores$Site==uID[1])
-BGVF[1,2]<-sum(SoilCores[xx,10])
+#########Changed method to use mean value, dropping missing values
+#Impute missing values for TC
+# xx<-which(is.na(SoilCores[,10]))
+# for(i in 1:length(xx)){
+#   if(SoilCores$Site[xx[i]]==SoilCores$Site[xx[i]-1]&SoilCores$Site[xx[i]]==SoilCores$Site[xx[i]+1]){
+#     SoilCores[xx[i],10]<-SoilCores[xx[i]-1,10]/2+SoilCores[xx[i]+1,10]/2
+#   } 
+#   if(SoilCores$Site[xx[i]]!=SoilCores$Site[xx[i]-1]&SoilCores$Site[xx[i]]==SoilCores$Site[xx[i]+1]){
+#     SoilCores[xx[i],10]<-SoilCores[xx[i]+1,10]
+#   }
+#   if(SoilCores$Site[xx[i]]==SoilCores$Site[xx[i]-1]&SoilCores$Site[xx[i]]!=SoilCores$Site[xx[i]+1]){
+#     SoilCores[xx[i],10]<-SoilCores[xx[i]-1,10]
+#   }
+# }
+# 
+# #check all imputed
+# print(length(which(is.na(SoilCores[,10]))))
 
-for(i in uID[-1]){
-  BGVF<-rbind(BGVF,matrix(NA,2,1))
-  BGVF[i,1]<-uID[i]
-  xx<-which(SoilCores$Site==uID[i])
-  BGVF[i,2]<-sum(SoilCores[xx,10])
+
+
+#Calculate BGVF as mean TC times 30 cm depth
+BGVF<-matrix(NA,1,3)
+BGVF[1,1]<-as.character(uID[1])
+xx<-which(SoilCores$Site==uID[1]&as.numeric(max.depth.cm)<=30)
+BGVF[1,3]<-mean(SoilCores[xx,12],na.rm=T)*30
+BGVF[1,2]<-as.character(SoilCores[xx[1],2])
+
+for(i in 2:length(uID)){
+  BGVF<-rbind(BGVF,matrix(NA,1,3))
+  BGVF[i,1]<-as.character(uID[i])
+  xx<-which(SoilCores$Site==uID[i]&as.numeric(max.depth.cm)<=30)
+  BGVF[i,3]<-mean(SoilCores[xx,12],na.rm=T)*30
+  BGVF[i,2]<-as.character(SoilCores[xx[1],2])
 }
+
+write.csv(BGVF,"BGVFbySite.csv",row.names=F)
+write.csv(SoilCores,"LUCAS_Soil_Cores_ER73_Baustian_201906_reformat.csv")
--- NPP_Calcs.r
+++ NPP_Calcs.r
@@ -1,3 +1,39 @@
 ANPP<-read.csv('LUCAS_ANPP_calcs_ER73_Stagg_201906.csv')
 BNPP<-read.csv('LUCAS_BNPP_calcs_ER73_Stagg_201906.csv')
 PrevCal<-read.csv('LUCAS_Stocks_ER73_Stagg_201906.csv')
+PrevID<-as.character(PrevCal[,1])
+
+#reconcile naming conventions
+xx<-which(PrevID=="BA")
+zz<-which(PrevID!="BA")
+PrevID[xx]<-'BA-01-04'
+PrevID2<-PrevID
+for(i in 1:length(zz)){
+  if(as.numeric(PrevID[zz[i]])>1000)PrevID2[zz[i]]<-as.character(paste('CRMS',as.character(as.numeric(PrevID[zz[i]])),sep=''))
+  if(as.numeric(PrevID[zz[i]])<1000)PrevID2[zz[i]]<-as.character(paste('CRMS0',as.character(as.numeric(PrevID[zz[i]])),sep=''))
+}
+
+#Get Marsh Type for ANPP BNPP
+ANPPtype<-rep(NA,dim(ANPP)[1])
+for(i in 1:dim(ANPP)[1]){
+  ANPPtype[i]<-PrevCal$Wetland.Subclass.LUCAS[which(PrevID2==ANPP[i,1])[1]]
+}
+
+BNPPtype<-rep(NA,dim(BNPP)[1])
+for(i in 1:dim(ANPP)[1]){
+  BNPPtype[i]<-PrevCal$Wetland.Subclass.LUCAS[which(PrevID2==BNPP[i,1])[1]]
+}
+
+#to start and end with winter (min living biomass), use Production listed under T3-T6
+#Camille already has calculated, so simply need to sum interval productions
+
+#########Notes on calculations#############
+#Changes calculated according to Smalley 1959
+#four cases based on Live.Change.g and Dead.Change.g being positive or negative, respectively
+#reduces to NPP = max(ANPP$Live.Change.g+max(ANPP$Dead.Change.g,0),0)
+#Change in live, plus any positive change in dead material
+#if result is negative, NPP is zero (it can't be negative in this algorithm)
+#conservative estimate of NPP, always zero or postive but probably underestimates losses and thus production when production is high
+#you can check this by uncommenting following two lines, they should print zeros
+#print(sum(as.numeric(ANPP$Interval.Production.g)-max(as.numeric(ANPP$Live.Change.g)+max(as.numeric(ANPP$Dead.Change.g),0),0),na.rm=T))
+#print(sum(as.numeric(BNPP$Interval.Production.g)-max(as.numeric(BNPP$Live.Change.g)+max(as.numeric(BNPP$Dead.Change.g),0),0),na.rm=T))
