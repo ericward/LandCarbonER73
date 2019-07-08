ddecd8b06c6fcd29761ec48347c1d2b3895704b6
ericward <eric.ward@gmail.com>
2019-07-07 19:28:11 -0500

    updating NPP calcs

--- NPP_Calcs.r
+++ NPP_Calcs.r
@@ -16,12 +16,12 @@ for(i in 1:length(zz)){
 #Get Marsh Type for ANPP BNPP
 ANPPtype<-rep(NA,dim(ANPP)[1])
 for(i in 1:dim(ANPP)[1]){
-  ANPPtype[i]<-PrevCal$Wetland.Subclass.LUCAS[which(PrevID2==ANPP[i,1])[1]]
+  ANPPtype[i]<-as.character(PrevCal$Wetland.Subclass.LUCAS[which(PrevID2==ANPP[i,1])[1]])
 }
 
 BNPPtype<-rep(NA,dim(BNPP)[1])
 for(i in 1:dim(ANPP)[1]){
-  BNPPtype[i]<-PrevCal$Wetland.Subclass.LUCAS[which(PrevID2==BNPP[i,1])[1]]
+  BNPPtype[i]<-as.character(PrevCal$Wetland.Subclass.LUCAS[which(PrevID2==BNPP[i,1])[1]])
 }
 
 #to start and end with winter (min living biomass), use Production listed under T3-T6
@@ -37,3 +37,35 @@ for(i in 1:dim(ANPP)[1]){
 #you can check this by uncommenting following two lines, they should print zeros
 #print(sum(as.numeric(ANPP$Interval.Production.g)-max(as.numeric(ANPP$Live.Change.g)+max(as.numeric(ANPP$Dead.Change.g),0),0),na.rm=T))
 #print(sum(as.numeric(BNPP$Interval.Production.g)-max(as.numeric(BNPP$Live.Change.g)+max(as.numeric(BNPP$Dead.Change.g),0),0),na.rm=T))
+
+ANPPsites<-unique(ANPP[,1])
+ANPPannual<-matrix(NA,length(ANPPsites)*5,4)
+for(i in 1:length(ANPPsites)){
+  xx<-which(ANPP[,1]==ANPPsites[i]&(ANPP$Event=='T3'|ANPP$Event=='T4'|ANPP$Event=='T5'|ANPP$Event=='T6'))
+  ANPPannual[((i-1)*5+1):((i-1)*5+5),1]<-ANPP[xx[1],1]
+  ANPPannual[((i-1)*5+1):((i-1)*5+5),2]<-ANPPtype[xx[1]]
+  for(j in 1:5){
+    ANPPannual[((i-1)*5+j),3]<-j  
+    xxx<-which(ANPP[,1]==ANPPsites[i]&(ANPP$Event=='T3'|ANPP$Event=='T4'|ANPP$Event=='T5'|ANPP$Event=='T6')&as.numeric(ANPP[,2])==j)
+    ANPPannual[((i-1)*5+j),4]<-sum(as.numeric(ANPP$Interval.Production.g[xxx]))
+  }
+}
+
+BNPPsites<-unique(BNPP[,1])
+BNPPannual<-matrix(NA,length(BNPPsites)*5,4)
+for(i in 1:length(BNPPsites)){
+  xx<-which(BNPP[,1]==BNPPsites[i]&(BNPP$Event=='T3'|BNPP$Event=='T4'|BNPP$Event=='T5'|BNPP$Event=='T6'))
+  BNPPannual[((i-1)*5+1):((i-1)*5+5),1]<-BNPP[xx[1],1]
+  BNPPannual[((i-1)*5+1):((i-1)*5+5),2]<-BNPPtype[xx[1]]
+  for(j in 1:5){
+    BNPPannual[((i-1)*5+j),3]<-j  
+    xxx<-which(BNPP[,1]==BNPPsites[i]&(BNPP$Event=='T3'|BNPP$Event=='T4'|BNPP$Event=='T5'|BNPP$Event=='T6')&as.numeric(BNPP[,2])==j)
+    BNPPannual[((i-1)*5+j),4]<-sum(as.numeric(BNPP$Interval.Production.g[xxx]))
+  }
+}
+
+colnames(ANPPannual)<-c('Site','Wetland_Type','Transect','ANPP.g')
+colnames(BNPPannual)<-c('Site','Wetland_Type','Transect','BNPP.g')
+
+write.csv(ANPPannual,'ANPPannual.csv',row.names=F)
+write.csv(BNPPannual,'BNPPannual.csv',row.names=F)
