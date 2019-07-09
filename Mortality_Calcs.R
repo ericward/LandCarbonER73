a600ce9b9b576d3173f11beac23abc39a4353192
Ward <eward@usgs.gov>
2019-07-09 15:13:09 -0500

    Corrected mortality calc for sign of productivity

--- Mortality_Calcs.R
+++ Mortality_Calcs.R
@@ -20,7 +20,7 @@ ANPPsites<-unique(ANPP[,1])
 for(i in 1:length(ANPPsites)){
   for(j in 1:5){
     xxx<-which(ANPP[,1]==ANPPsites[i]&(ANPP$Event=='T3'|ANPP$Event=='T4'|ANPP$Event=='T5'|ANPP$Event=='T6')&as.numeric(ANPP[,2])==j)
-    zzz<-cbind(as.numeric(ANPP$Live.Change.g[xxx])/as.numeric(ANPP$Plot.Area.m2[xxx]),as.numeric(ANPP$Dead.Change.g[xxx])/as.numeric(ANPP$Plot.Area.m2[xxx]),rep(0,length(xxx)))
+    zzz<-cbind(-as.numeric(ANPP$Live.Change.g[xxx])/as.numeric(ANPP$Plot.Area.m2[xxx]),as.numeric(ANPP$Dead.Change.g[xxx])/as.numeric(ANPP$Plot.Area.m2[xxx]),rep(0,length(xxx)))
     AM[((i-1)*5+j),4]<-sum(apply(zzz,1,max))*0.5
   }
 }
@@ -29,7 +29,7 @@ BNPPsites<-unique(BNPP[,1])
 for(i in 1:length(BNPPsites)){
   for(j in 1:5){
     xxx<-which(BNPP[,1]==BNPPsites[i]&(ANPP$Event=='T3'|BNPP$Event=='T4'|BNPP$Event=='T5'|BNPP$Event=='T6')&as.numeric(BNPP[,2])==j)
-    zzz<-cbind(as.numeric(BNPP$Live.Change.g[xxx])/as.numeric(BNPP$Core.Area.m2[xxx]),as.numeric(BNPP$Dead.Change.g[xxx])/as.numeric(BNPP$Core.Area.m2[xxx]),rep(0,length(xxx)))
+    zzz<-cbind(-as.numeric(BNPP$Live.Change.g[xxx])/as.numeric(BNPP$Core.Area.m2[xxx]),as.numeric(BNPP$Dead.Change.g[xxx])/as.numeric(BNPP$Core.Area.m2[xxx]),rep(0,length(xxx)))
     BM[((i-1)*5+j),4]<-sum(apply(zzz,1,max))*0.5
   }
 }
